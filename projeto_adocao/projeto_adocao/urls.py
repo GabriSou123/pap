e08aa8142dcece2ef98358c8349b5eb0714d4c61
Gabriel <sousagabrielsantiago@gmail.com>
2025-10-01 16:48:23 +0100

    Atualização pagina adotar

--- projeto_adocao/projeto_adocao/urls.py
+++ projeto_adocao/projeto_adocao/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path('contactos/', views.contactos, name='contactos'),
     path('sobreNos/', views.sobreNos, name='sobreNos'),
     path('cao1/', views.cao1, name='cao1'),
-    path('cao2/', views.cao2, name='cao2'),
     path('gato4/', views.gato4, name='gato4'),
     
 ]
