5da8cb30e743dbdfd7325a4635c891b2f50ddebe
Gabriel <sousagabrielsantiago@gmail.com>
2025-10-01 11:45:52 +0100

    Atualização no home

--- projeto_adocao/app_adocao/models.py
+++ projeto_adocao/app_adocao/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Animal(models.Model):
+    nome = models.CharField(max_length=100)
+    idade = models.IntegerField()
+    especie = models.CharField(max_length=50)
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.nome
