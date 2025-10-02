1aed03e21d9bc150da723a233f1e6336d0f89e29
Gabriel <sousagabrielsantiago@gmail.com>
2025-10-02 12:48:24 +0100

    Alterações
    02/10/2025

--- projeto_adocao/app_adocao/models.py
+++ projeto_adocao/app_adocao/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Animal(models.Model):
     nome = models.CharField(max_length=100)
-    idade = models.IntegerField()
+    idade = models.IntegerField(max_length=2)
     especie = models.CharField(max_length=50)
     descricao = models.TextField()
     imagem = models.ImageField(upload_to='imgAnimal/')
