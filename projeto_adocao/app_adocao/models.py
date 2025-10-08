ac5c9f038b838da3b6de7c85030ecd4eba96a8a4
Gabriel <sousagabrielsantiago@gmail.com>
2025-10-08 19:02:57 +0100

    Sign in e sign up
    Página sign in e sign up configuradas

--- projeto_adocao/app_adocao/models.py
+++ projeto_adocao/app_adocao/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Animal(models.Model):
     nome = models.CharField(max_length=100)
@@ -9,3 +10,12 @@ class Animal(models.Model):
 
     def __str__(self):
         return self.nome
+    
+class PerfilUtilizador(models.Model):
+    usename = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.CharField(max_length=20)
+    palavraPasse = models.CharField(max_length=20)
+    confirmarPalavraPasse = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.user.username
--- projeto_adocao/app_adocao/views.py
+++ projeto_adocao/app_adocao/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from .models import Animal
-from django.contrib.auth import login
+from.models import PerfilUtilizador
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
 
 
 def index(request):
@@ -24,6 +26,28 @@ def animaisadc(request, animal_id):
 
 def sign_up(request):
     return render(request,'site/sign_up.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        palavraPasse = request.POST['palavraPasse']
+        confirmarPalavraPasse = request.POST['confirmarPalavraPasse']
+        if palavraPasse == confirmarPalavraPasse:
+            User.objects.create_user(username=username, email=email, palavraPasse=palavraPasse)
+            return redirect('sign_in')
+        else:
+            return render(request, 'sign_up.html', {'error': 'As palavras-passe não coincidem'})
+    return render(request, 'sign_up.html')
+
 
 def sign_in(request):
     return render(request,'site/sign_in.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        palavraPasse = request.POST['palavraPasse']
+        user = authenticate(request, username=username, palavraPasse=palavraPasse)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'sign_in.html', {'error': 'Erro na palavra passe ou nome de utilizador.'})
+    return render(request, 'sign_in.html')
