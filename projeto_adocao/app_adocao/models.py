9576e1de59ef675460a221e0f2407671b38eba47
Gabriel <sousagabrielsantiago@gmail.com>
2025-10-09 12:49:09 +0100

    It worked

--- projeto_adocao/app_adocao/models.py
+++ projeto_adocao/app_adocao/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 class Animal(models.Model):
     nome = models.CharField(max_length=100)
@@ -14,15 +12,10 @@ class Animal(models.Model):
         return self.nome
     
 class PerfilUtilizador(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    usename = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.CharField(max_length=20)
+    palavraPasse = models.CharField(max_length=20)
+    confirmarPalavraPasse = models.CharField(max_length=20)
 
     def __str__(self):
         return self.user.username
-
-
-@receiver(post_save, sender=User)
-def criar_ou_atualizar_perfil(sender, instance, created, **kwargs):
-    if created:
-        PerfilUtilizador.objects.create(user=instance)
-    else:
-        instance.perfilutilizador.save()
